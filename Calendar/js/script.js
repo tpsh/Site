e0bfd0f0c70fa6a389c69d40a7347b5ee2338cb6
ChristNastya <alina_perecz@mail.ru>
2016-03-24 03:50:27 +1000

    add Calendar div

---
+++ Calendar/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+    $('#calendar').fullCalendar({
+        lang : "ru",
+        // put your options and callbacks here
+        dayClick: function(date, jsEvent, view) {
+
+        alert('Clicked on: ' + date.format());
+
+        alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+
+        alert('Current view: ' + view.name);
+
+        // change the day's background color just for fun
+        $(this).css('background-color', 'red');
+
+
+        }
+    
+    });
+
+    $('#calendar_day').fullCalendar({
+            defaultView : 'basicWeek' ,
+            lang : "ru",
+            contentHeight: 150
+        // put your options and callbacks here
+    });
+
+
+
+    $('.fc-day-number').append('<div>Test</div>');
+
+
+}); 
---
+++ Template/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        lang : "ru",
+        header : {
+            left: "",
+            center: "title",
+            right: ""
+        }
+    });
+
+    $('#calendar_day').fullCalendar({
+            defaultView : 'basicWeek' ,
+            lang : "ru",
+            contentHeight: 150,
+            header: "false"
+        // put your options and callbacks here
+    });
+
+    $('.fc-day-number').append('<div>Test</div>');
+
+    var date = new Date ;
+    monthA = 'января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря'.split(',');
+    document.getElementById("my_date").innerHTML = ('0' + date.getDate()).slice(-2) + ' ' + (monthA[date.getMonth() + 1])  + ' ' + date.getFullYear()
+;
+
+}); 
+
