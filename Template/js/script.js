c00063349784943307cab64fa811e2241b115d10
YanaTimofeyeva <yanuchka-s@yandex.ru>
2016-03-25 11:06:59 +0300

    lets add some JQuery

--- Template/js/script.js
+++ Template/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+
+var date = new Date ;
+    monthA = 'января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря'.split(',');
+    document.getElementById("my_date").innerHTML = ('0' + date.getDate()).slice(-2) + ' ' + (monthA[date.getMonth() + 1])  + ' ' + date.getFullYear()
+;
+
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
@@ -22,12 +28,9 @@ $(document).ready(function() {
         // put your options and callbacks here
     });
 
-    $('.fc-day-number').append('<div></div>');
+    $('.fc-day').append('<div class=""><img src="img/weather/big_cloud.png" alt="cloud"></div>');
+    $('.fc-day').append('<div><h1>+40 &deg;C</h1></div>');
+    $('.fc-day').append('<div><p>Скорость ветра 5 м/c</div>');
 
-    var date = new Date ;
-    monthA = 'января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря'.split(',');
-    document.getElementById("my_date").innerHTML = ('0' + date.getDate()).slice(-2) + ' ' + (monthA[date.getMonth() + 1])  + ' ' + date.getFullYear()
-;
 
 }); 
-
